305f7b122a88960414dc14fc84aee45576dfe0a9
An Tran <haantran96@gmail.com>
2021-03-14 23:43:24 +0200

    adding models, focal loss

--- method.py
+++ method.py
@@ -3,38 +3,53 @@ import pickle
 
 import tools.file_io as file_io
 from tools.argument_parsing import get_argument_parser
-from data_handler.dataset import ASDataset, get_data_loader
+from data_handler.dataset import ASDataset, get_data_loader, split_dataset
 import logging
 from modules.triresnet import TridentResNet
-from modules.resnest50 import ResNeSt50
+from modules.triresnet_2 import TridentResNet2
+from modules.resnet_d import ResNet_d
+from modules.focal_loss import FocalLoss
 import torch
 import torch.nn as nn
 import numpy as np
 import time
 import sklearn
+import matplotlib.pyplot as plt
 
 args = get_argument_parser().parse_args()
 
 logging.basicConfig(
-    filename=args.log_dir,
+    filename=f"logs/log_{args.job_id}",
     level=logging.DEBUG,
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
 log = logging.getLogger(__name__)
 
+def plot_util(epochs, train, val, title):
+    epochs = range(epochs)
+    plt.plot(epochs, loss_train, 'g', label=f'Training {title}')
+    plt.plot(epochs, loss_val, 'b', label=f'Validation {title}')
+    plt.title(f'Training and Validation {title}')
+    plt.xlabel('Epochs')
+    plt.ylabel(title)
+    plt.legend()
+    plt.savefig(f"{args.job_id}_{title}.png")
+
 def dataset(settings_data, hpram_settings, is_testing):
     batch_size = hpram_settings["batch_size"]
     num_workers = hpram_settings["num_workers"]
     if not is_testing:
-        train_set = ASDataset(
-            split="train",
-            load_into_memory=False,
-            data_features_dir=settings_data["data_features_dir"],
-            data_parent_dir=settings_data["data_parent_dir"],
-            meta_parent_dir=settings_data["meta_parent_dir"],
-            meta_dir=settings_data["meta_train_dir"],
-        ) 
+        train_set, val_set = split_dataset(
+            ASDataset(
+                split="train",
+                load_into_memory=False,
+                data_features_dir=settings_data["data_features_dir"],
+                data_parent_dir=settings_data["data_parent_dir"],
+                meta_parent_dir=settings_data["meta_parent_dir"],
+                meta_dir=settings_data["meta_train_dir"],
+            ) 
+        )
         train_loader = get_data_loader(
             train_set,
             batch_size=batch_size,
@@ -43,6 +58,7 @@ def dataset(settings_data, hpram_settings, is_testing):
             num_workers = num_workers,
         )
 
+        """
         val_set = ASDataset(
             split="val",
             load_into_memory=False,
@@ -51,7 +67,7 @@ def dataset(settings_data, hpram_settings, is_testing):
             meta_parent_dir=settings_data["meta_parent_dir"],
             meta_dir=settings_data["meta_val_dir"],
         ) 
-
+        """
         val_loader = get_data_loader(
             val_set,
             batch_size=batch_size,
@@ -98,32 +114,41 @@ def dataset(settings_data, hpram_settings, is_testing):
 
 
 def _do_training(model, optimizer, loss_fn, scheduler, train_loader, val_loader, settings, device):
-    train_loss = []
-    val_loss = []
+    best_acc = 0
+    best_epoch = 0
+    patience = 0
+    train_losses = []
+    val_losses = []
+    train_accs = []
     val_accs = []
-    best_loss = float("inf")
     for epoch in range(settings["epochs"]):
+        train_loss = []
+        val_loss = []
+        val_accs = []
         start_time = time.time()
         model.train() 
+        pred_train = []
+        targets_train = []
         for feature,target in train_loader:
             feature = feature.to(device)
             target = target.to(device)
-
             optimizer.zero_grad()
             y_hat = model(feature)
 
             loss = loss_fn(y_hat, target.squeeze(1))
 
+            pred_train.extend(y_hat.argmax(axis=1).cpu().numpy().tolist())
+            targets_train.extend(target.squeeze(1).cpu().numpy().tolist())
+
             loss.backward()
             optimizer.step()
             train_loss.append(loss.item())
-            if loss < best_loss:
-                best_loss = loss.item()
-                torch.save(model.state_dict(), f'save_models/{settings["save_model"]}')
-
-        
+        train_acc = sklearn.metrics.accuracy_score(pred_train,targets_train)
         model.eval()
         with torch.no_grad():
+            pred = []
+            targets = []
+            val_acc = 0
             for feature,target in val_loader:
                 feature = feature.to(device)
                 target = target.to(device)
@@ -131,12 +156,32 @@ def _do_training(model, optimizer, loss_fn, scheduler, train_loader, val_loader,
 
                 loss = loss_fn(y_hat, target.squeeze(1))
                 val_loss.append(loss.item())
-                val_acc = (y_hat.argmax(axis=1) == target.squeeze(1)).float().mean()
-                val_accs.append(val_acc.item())
+                pred.extend(y_hat.argmax(axis=1).cpu().numpy().tolist())
+                targets.extend(target.squeeze(1).cpu().numpy().tolist())
+            val_acc = sklearn.metrics.accuracy_score(pred,targets)
+            scheduler.step(np.mean(val_loss))
+        
+        # Append to the big lists
+        train_losses.append(np.mean(train_loss))
+        val_losses.append(np.mean(val_loss))
+        train_accs.append(train_acc)
+        val_accs.append(val_acc)
 
-
-        #scheduler.step(np.mean(val_loss))
-        log.info(f"Epoch {epoch}: Train loss: {np.mean(train_loss)} | Val loss: {np.mean(val_loss)} | Val acc: {np.mean(val_accs)} | Time: {time.time()-start_time}")
+        # Early stopping
+        if val_acc > best_acc:
+            patience = 0
+            best_loss = val_acc
+            best_epoch = epoch
+            torch.save(model.state_dict(), f'save_models/{settings["save_model"]}')
+        else:
+            patience += 1
+        if patience > settings['patience']:
+            log.info(f'Early stopping, best loss at {best_epoch}')
+            plot_util(epoch, train_losses, val_losses, "Loss")
+            plot_util(epoch, train_accs, val_s, "Loss")
+            return
+        else:
+            log.info(f"Epoch {epoch}: Train loss: {np.mean(train_loss)} | Train acc: {train_acc} |Val loss: {np.mean(val_loss)} | Val acc: {val_acc} | Time: {time.time()-start_time}")
 
 def _do_evaluation(model,eva_loader, settings, device):
     checkpoint = torch.load(f'save_models/{settings["save_model"]}', map_location=device)
@@ -157,7 +202,7 @@ def _do_evaluation(model,eva_loader, settings, device):
 
             pred.extend(y_hat.argmax(axis=1).cpu().numpy().tolist())
             targets.extend(target.squeeze(1).cpu().numpy())
-
+    log.info("Accuracy score")
     log.info(sklearn.metrics.accuracy_score(pred,targets))
     return pred, target 
 
@@ -168,7 +213,7 @@ def main():
     file_ext = args.file_ext
     verbose = args.verbose
     job_id = args.job_id
-
+    
     settings = file_io.load_yaml_file(Path(
         file_dir, f'{config_file}.{file_ext}'))
 
@@ -186,27 +231,39 @@ def main():
                         pretrained=model_settings["pretrained"],
                         n_classes=model_settings["n_classes"],
                         )
+    elif model_settings["model_name"] == "triresnet_2":
+        model = TridentResNet2(pretrained=False)
+    elif model_settings["model_name"] == "resnet_d":
+        model = ResNet_d()
     model.to(device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=hpram_settings["optimizer"]["lr"])
-    #scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", patience=5, factor=0.3, verbose=True)
-    loss_fn = nn.CrossEntropyLoss()
+    log.info(f"Model saved dir: {hpram_settings['save_model']}")
+    if settings["flow"]['continue_training']:
+        log.info('Loading model checkpoint')
+        checkpoint = torch.load(f'save_models/{hpram_settings["save_model"]}', map_location=device)
+        model.load_state_dict(checkpoint)
 
+    optimizer = torch.optim.Adam(model.parameters(), lr=hpram_settings["optimizer"]["lr"])
+    #scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=0)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", patience=5, factor=0.3, verbose=True)
+    if hpram_settings['loss'] == "focal":
+        loss_fn = FocalLoss(gamma=2)
+    else:
+        loss_fn = nn.CrossEntropyLoss()
+
+    train_loader, val_loader, eva_loader = dataset(dataset_settings, hpram_settings, is_testing=False)
+    log.info(f'Training data shape: {len(train_loader)}')
+    log.info(f'Validation data shape: {len(val_loader)}')
+    log.info(f'Evaluation data shape: {len(eva_loader)}')
     if settings["flow"]["training"]:
         log.info("Loading training and validation data")
-        train_loader, val_loader, eva_loader = dataset(dataset_settings, hpram_settings, is_testing=False)
-        log.info(f'Training data shape: {len(train_loader)}')
-        log.info(f'Validation data shape: {len(val_loader)}')
-        log.info(f'Evaluation data shape: {len(eva_loader)}')
-        
         _do_training(model,optimizer, loss_fn, scheduler, train_loader, 
                     val_loader, hpram_settings, device)
-        #_do_evaluation(model, eva_loader, hpram_settings, device)
+    
+    if settings["flow"]["testing"]:
+        _do_evaluation(model, eva_loader, hpram_settings, device)
         log.info(f'Predicting on Evaluation set')
         predictions, targets = _do_evaluation(model, eva_loader, hpram_settings, device)
-
-    if settings["flow"]["testing"]:
-        testing_loader = dataset(dataset_settings, hpram_settings, is_testing=True)
 
 if __name__ == '__main__':
     main()
---
+++ modules/resnet_d.py
@@ -0,0 +1,68 @@
+# Source: https://github.com/MihawkHu/DCASE2020_task1/blob/2384da9d357bc6be9648701a0ae0c4b4be285e36/task1a/10class/resnet/resnet.py
+from torch import Tensor, cat, reshape, rand
+from torch.nn import Module, Sequential, Conv2d, MaxPool2d, BatchNorm2d, ReLU,\
+                        Dropout2d, Linear, AdaptiveAvgPool2d
+from torchvision.models import resnet18
+from torch.optim import Adam
+
+__author__ = 'Duy Vu'
+
+class ResNet_d(Module):
+    def __init__(self, num_classes=10) -> None:
+        super(ResNet_d, self).__init__()
+        
+        # Get architecture of ResNet-18 but without the last pooling layer and FC layer
+        self.res_net_1 = resnet18(pretrained=False) 
+        self.res_net_2 = resnet18(pretrained=False)
+        
+        self.res_net_1 = Sequential(*(list(self.res_net_1.children())[:-2]))
+        self.res_net_2 = Sequential(*(list(self.res_net_2.children())[:-2]))
+
+        self.conv_out_1 = Sequential(
+            Conv2d(in_channels=512,
+                   out_channels=64,
+                   kernel_size=(1, 1),
+                   stride=(1, 1)),
+            BatchNorm2d(num_features=64),
+            ReLU()
+        )
+        
+        self.conv_out_2 = Sequential(
+            Conv2d(in_channels=64,
+                   out_channels=num_classes,
+                   kernel_size=(1, 1),
+                   stride=(1, 1)),
+            BatchNorm2d(num_features=num_classes),
+            ReLU()
+        )
+        
+        self.batch_norm = BatchNorm2d(num_classes)
+        self.global_pooling = AdaptiveAvgPool2d(1)  # Global average pooling
+        #self.linear = Linear(in_features=10, out_features=10)
+    
+
+    def forward(self, 
+                X: Tensor) -> Tensor:
+        X = X.float()
+        X = X if X.ndimension() == 4 else X.unsqueeze(1)
+        
+        # ResNet
+        batch, channels, mel_bins, timestep = X.shape
+        out_res_1 = self.res_net_1(X[:, :, 0:mel_bins//2, :])
+        out_res_2 = self.res_net_2(X[:, :, mel_bins//2:, :])
+        out = cat((out_res_1, out_res_2), axis=2)
+
+        out = self.conv_out_1(out)
+        out = self.conv_out_2(out)
+        
+        out = self.batch_norm(out)
+        
+        out = self.global_pooling(out)
+        out = out.squeeze()
+
+        return out
+
+if __name__ == "__main__":
+    dnn = ResNet_d()
+    x = rand(4, 3, 128, 423) 
+    y = dnn(x)
